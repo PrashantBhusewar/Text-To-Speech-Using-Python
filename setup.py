eb09469e5c761c4c13a122a1ac99ee3deefaa570
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-03 11:19:36 +0530

    logger & Exception

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Text-To_Speech",
+    version="0.0.1",
+    author="Prashant",
+    author_email="prashantbhusewar1599@gmail.com",
+    packages=find_packages()
+)
